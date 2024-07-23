994a421a0964fd034035c24ad02186f81b2683bc
Ryan Luong <ryanluong83@gmail.com>
2024-07-23 12:38:22 -0700

    Installed MUI X Charts

--- src/app/agents/pick-rates/page.tsx
+++ src/app/agents/pick-rates/page.tsx
@@ -26,15 +26,17 @@ const PickRates = () => {
         <>
         <div className="flex flex-col w-[800px] h-[600px] bg-gray-200 border-2 border-dashed border-gray-500 mx-auto my-4">
                 <div className="flex flex-row border-2 border-dashed border-blue-500 p-2"> 
-                <MultiSelectDropdown label="Season" options={SEASONS} selectedOptions={selectedSeasons} onSelectionChange={handleSeasonsChange}/>
-                <MultiSelectDropdown label="Stage" options={STAGES} selectedOptions={selectedStages} onSelectionChange={handleStagesChange}/>
-                <MultiSelectDropdown label="Map" options={MAPS} selectedOptions={selectedMaps} onSelectionChange={handleMapsChange}/>
+                    <MultiSelectDropdown label="Season" options={SEASONS} selectedOptions={selectedSeasons} onSelectionChange={handleSeasonsChange}/>
+                    <MultiSelectDropdown label="Stage" options={STAGES} selectedOptions={selectedStages} onSelectionChange={handleStagesChange}/>
+                    <MultiSelectDropdown label="Map" options={MAPS} selectedOptions={selectedMaps} onSelectionChange={handleMapsChange}/>
                 </div>
                 <div className="flex flex-row border-2 border-dashed border-blue-500 p-2">
-                <SearchBar label="Team" nameList={selectedTeams}/>
-                <SingleSelectDropdown label="Player or Team View" options={VIEWMODE} defaultOption="Player"/>  
+                    <SearchBar label="Team" nameList={selectedTeams}/>
+                    <SingleSelectDropdown label="Player or Team View" options={VIEWMODE} defaultOption="Player"/>  
                     </div> 
-                <div></div>
+                <div>
+                    
+                </div>
             </div>
         </>
     )
